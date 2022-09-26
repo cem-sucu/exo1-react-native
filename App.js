a9dab7ceba70273acd588efc06aec8c367225bf8
cem-sucu <cems.cems.17@gmail.com>
2022-09-26 14:40:57 +0200

    composant Text et View

--- App.js
+++ App.js
@@ -1,24 +1,10 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { View, Text } from 'react-native'
+import React from 'react'
 
 export default function App() {
-    const [count, setCount] = useState(0);
-    return (
-        <View style={styles.container}>
-            <Text style={styles.counter}>{count}</Text>
-            <Button onPress={() => setCount(count + 1)} title="+1" />
-        </View>
-    );
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    counter: {
-        fontSize: 40,
-    },
-});
