4ec6f6f5a0deabe4ec7e8b095034042a5facedfa
cem-sucu <cems.cems.17@gmail.com>
2022-09-26 15:19:04 +0200

    inline css

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
 export default function App() {
-  return (
-    <View>
-      <Text>hello</Text>
-    </View>
-  )
+    return (
+        <View>
+            <Text style={styles.container}>hello</Text>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        color: "red",
+        marginTop: 50,
+        flexDirection: "row",
+        fontSize: 20,
+    },
+});
