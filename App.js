d2a29ecf96ce72479bff5a8de8d88c31b24e02b9
cem-sucu <cems.cems.17@gmail.com>
2022-09-29 10:22:05 +0200

    afficher le contenu d'un objet

--- App.js
+++ App.js
@@ -2,46 +2,57 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 export default function App() {
-    const [name, setName] = useState();
-    const [age, setAge] = useState();
-
-    
+    const obj = [
+        {
+            id: 1,
+            name: "stan",
+            age: 45,
+        },
+        {
+            id: 2,
+            name: "francine",
+            age: 25,
+        },
+        {
+            id: 3,
+            name: "hayley",
+            age: 15,
+        },
+        {
+            id: 4,
+            name: "steve",
+            age: 85,
+        },
+        {
+            id: 5,
+            name: "roger",
+            age: 45,
+        },
+        {
+            id: 6,
+            name: "klaus",
+            age: 35,
+        },
+    ];
+    const [family, setFamily] = useState(obj);
+    console.log(family);
 
     return (
         <View style={styles.wrapper}>
-            <Text style={styles.text}>Nom : {name}</Text>
-            <TextInput
-                placeholder="indiquez votre nom"
-                style={styles.textInput}
-                onChangeText={(val) => setName(val)}
-            />
-            <Text style={styles.text}>Age : {age} ans</Text>
-            <TextInput
-                placeholder="indiquez votre age"
-                style={styles.textInput}
-                onChangeText={(val) => setAge(val)}
-                keyboardType="numeric"
-            />
+            {family.map((member) => {
+                return (
+                    <View key={member.id} style={styles.viewList}>
+                        <Text style={styles.text}>Nom : {member.name}</Text>
+                        <Text style={styles.text}>Age : {member.age}</Text>
+                    </View>
+                );
+            })}
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    wrapper: {
-        marginTop: 50,
-        backgroundColor: "red",
-        alignItems: "center",
-        height: 220,
-    },
-    textInput: {
-        height: 40,
-        borderColor: "grey",
-        borderWidth: 1,
-        padding: 10,
-        margin: 9,
-        width: "90%",
-    },
-    text: {
-        fontSize: 20,
-    },
+    wrapper: { padding: 20 },
+    viewList: { marginTop: 30, backgroundColor: "purple", padding: 19 },
+    text: { color: "white", fontSize: 24 },
 });
